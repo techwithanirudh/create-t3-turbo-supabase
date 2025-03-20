d8e5766dc0a01cae730137155892774833b23528
Pedro Shin <pedromaschio.shin@gmail.com>
2025-03-20 14:17:15 -0300

    Ç

--- setup-project.sh
+++ setup-project.sh
@@ -169,7 +169,7 @@ PROJECT_NAME=${PROJECT_NAME:-$(generate_project_name)}
 # Set project directory
 PROJECT_DIR="$HOME/Desktop/${PROJECT_NAME}"
 
-REPO_URL="https://github.com/pedromshin/create-t3-turbo-supabase"
+REPO_URL="https://github.com/pedromshin/create-turbo-stack"
 DB_PASSWORD=$(openssl rand -base64 16)
 JWT_SECRET=$(openssl rand -base64 32)
 
